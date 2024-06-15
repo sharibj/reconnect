69fa9f658bc57b3b7fc3bd324aebfc2a3e0994c8
Sharib Jafari <sharib.jafari.extern@idealo.de>
2024-06-15 11:52:41 +0200

    Add all tests for GroupDomainService

--- reconnect/src/main/java/domain/group/GroupDomainService.java
+++ reconnect/src/main/java/domain/group/GroupDomainService.java
@@ -2,7 +2,6 @@ package domain.group;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 public class GroupDomainService {
@@ -14,31 +13,71 @@ public class GroupDomainService {
         this.repository = repository;
     }
 
-    public Group addGroup(String name) throws IOException {
-        return addGroup(name, DEFAULT_FREQUENCY);
+    public void addGroup(String name) throws IOException {
+        addGroup(name, DEFAULT_FREQUENCY);
     }
 
-    public Group addGroup(String name, Integer frequencyInDays) throws IOException {
+    public void addGroup(String name, Integer frequencyInDays) throws IOException {
         if (repository.findByName(name).isPresent()) {
             throw new IOException("Group with name " + name + " already exists");
         }
-        return repository.save(Group.builder().name(name).frequencyInDays(frequencyInDays).build());
+        repository.save(Group.builder().name(name).frequencyInDays(frequencyInDays).build());
     }
 
-    public Group removeGroupById(String id) {
-
-        //@TODO check if id exists
-        return repository.deleteById(id);
+    public void removeGroupById(String id) throws IOException {
+        if (repository.findById(id).isEmpty()) {
+            throw new IOException("Group with id = " + id + " doesn't exist");
+        }
+        repository.deleteById(id);
     }
 
-    public Group removeGroupByName(String name) {
-        //@TODO check if id exists
-        Optional<Group> group = repository.findByName(name);
-        return group.map(value -> repository.deleteById(value.getId())).orElse(null);
+    public void removeGroupByName(String name) throws IOException {
+        if (repository.findByName(name).isEmpty()) {
+            throw new IOException("Group with name = " + name + " doesn't exist");
+        }
+        repository.deleteByName(name);
     }
 
     public Set<Group> getAll() {
-        //TODO add logic for de-dupe maybe
         return new HashSet<>(repository.findAll());
+    }
+
+    public Group getById(final String id) throws IOException {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new IOException("Group with id = " + id + " does not exist."));
+    }
+
+    public Group getByName(final String name) throws IOException {
+        return repository
+                .findByName(name)
+                .orElseThrow(() -> new IOException("Group with name = " + name + " does not exist."));
+    }
+
+    public void updateByName(final String name, final int frequencyInDays) throws IOException {
+        Group group = getByName(name);
+        if (group.getFrequencyInDays().equals(frequencyInDays)) {
+            return;
+        }
+        Group updatedGroup = Group.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .frequencyInDays(frequencyInDays)
+                .build();
+        repository.save(updatedGroup);
+    }
+
+    public void updateById(final String id, final String name, final int frequencyInDays) throws IOException {
+        Group group = getById(id);
+        if (group.getName().equals(name) && group.getFrequencyInDays().equals(frequencyInDays)) {
+            return;
+        }
+        Group updatedGroup = Group.builder()
+                .id(group.getId())
+                .name(name)
+                .frequencyInDays(frequencyInDays)
+                .build();
+        repository.save(updatedGroup);
+
     }
 }
--- reconnect/src/test/java/domain/group/GroupDomainServiceTest.java
+++ reconnect/src/test/java/domain/group/GroupDomainServiceTest.java
@@ -2,9 +2,13 @@ package domain.group;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,6 +32,7 @@ class GroupDomainServiceTest {
     @InjectMocks
     GroupDomainService service;
 
+    //region add group
     @Test
     void whenAddGroupWithName_thenSaveWithDefaultFrequency() throws IOException {
         // given
@@ -58,7 +63,7 @@ class GroupDomainServiceTest {
     }
 
     @Test
-    void whenAddGroupWithExistingName_thenThrowError() {
+    void whenAddGroupWithExistingName_thenThrowErrorOnSave() {
         // when
         Mockito.when(repository.findByName("family")).thenReturn(Optional.ofNullable(Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build()));
 
@@ -68,6 +73,7 @@ class GroupDomainServiceTest {
                 () -> service.addGroup("family")
         );
     }
+
     @Test
     void whenAddGroupWithExistingNameAndCustomFrequency_thenThrowError() {
         // when
@@ -79,4 +85,207 @@ class GroupDomainServiceTest {
                 () -> service.addGroup("family", 100)
         );
     }
+    //endregion
+
+    //region remove group
+
+    @Test
+    void whenCalledWithIdOfAnExistingGroup_thenRemoveGroup() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findById(group.getId())).thenReturn(Optional.of(group));
+
+        // when
+        service.removeGroupById(group.getId());
+
+        // then
+        Mockito.verify(repository, Mockito.times(1)).deleteById(group.getId());
+
+    }
+
+    @Test
+    void whenCalledWithIdOfANonExistingGroup_thenThrowException() throws IOException {
+        // given
+        String id = "id";
+
+        // when
+        Mockito.when(repository.findById(id)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(
+                IOException.class,
+                () -> service.removeGroupById(id)
+        );
+
+    }
+
+
+    @Test
+    void whenCalledWithNameOfAnExistingGroup_thenRemoveGroup() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findByName(group.getName())).thenReturn(Optional.of(group));
+
+        // when
+        service.removeGroupByName(group.getName());
+
+        // then
+        Mockito.verify(repository, Mockito.times(1)).deleteByName(group.getName());
+
+    }
+
+    @Test
+    void whenCalledWithNameOfANonExistingGroup_thenThrowException() throws IOException {
+        // given
+        String name = "family";
+
+        // when
+        Mockito.when(repository.findByName(name)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(
+                IOException.class,
+                () -> service.removeGroupByName(name)
+        );
+
+    }
+
+    //endregion
+
+    //region update group
+
+    @Test
+    void whenGroupWithNameExists_thenUpdateGroupByName() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findByName(group.getName())).thenReturn(Optional.of(group));
+        // when
+        service.updateByName("family", 20);
+        // then
+        Mockito.verify(repository, Mockito.times(1)).save(groupCaptor.capture());
+        Group capturedGroup = groupCaptor.getValue();
+        assertEquals(20, capturedGroup.getFrequencyInDays().intValue());
+    }
+
+    @Test
+    void whenGroupDoesNotChange_thenDoNotUpdateGroupByName() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findByName(group.getName())).thenReturn(Optional.of(group));
+        // when
+        service.updateByName("family", DEFAULT_FREQUENCY);
+        // then
+        Mockito.verify(repository, Mockito.times(0)).save(any());
+    }
+
+    @Test
+    void whenGroupWithNameDoesNotExist_thenThrowExceptionOnUpdate() {
+        // given
+        String groupName = "family";
+        // when
+        Mockito.when(repository.findByName(groupName)).thenReturn(Optional.empty());
+        // then
+        assertThrows(IOException.class, () -> service.updateByName(groupName, DEFAULT_FREQUENCY));
+    }
+
+
+    @Test
+    void whenGroupWithIdExists_thenUpdateGroupById() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findById(group.getId())).thenReturn(Optional.of(group));
+        // when
+        service.updateById(group.getId(), "friends", 20);
+        // then
+        Mockito.verify(repository, Mockito.times(1)).save(groupCaptor.capture());
+        Group capturedGroup = groupCaptor.getValue();
+        assertEquals(group.getId(), capturedGroup.getId());
+        assertEquals("friends", capturedGroup.getName());
+        assertEquals(20, capturedGroup.getFrequencyInDays().intValue());
+    }
+
+    @Test
+    void whenGroupDoesNotChange_thenDoNotUpdateGroupById() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findById(group.getId())).thenReturn(Optional.of(group));
+        // when
+        service.updateById(group.getId(), "family", DEFAULT_FREQUENCY);
+        // then
+        Mockito.verify(repository, Mockito.times(0)).save(any());
+    }
+
+    @Test
+    void whenGroupWithIdDoesNotExist_thenThrowExceptionOnUpdate() {
+        // given
+        String groupId = "nonExistingId";
+        // when
+        Mockito.when(repository.findById(groupId)).thenReturn(Optional.empty());
+        // then
+        assertThrows(IOException.class, () -> service.updateById(groupId, "family", DEFAULT_FREQUENCY));
+    }
+
+
+    //endregion
+
+    //region get group
+
+    @Test
+    void whenGetAll_thenReturnAllGroups() {
+        // given
+        Group familyGroup = Group.builder().name("family").frequencyInDays(1).build();
+        Group friendsGroup = Group.builder().name("friends").frequencyInDays(1).build();
+
+        Mockito.when(repository.findAll()).thenReturn(List.of(familyGroup, friendsGroup, familyGroup));
+        // when
+        Set<Group> allGroups = service.getAll();
+
+        // then
+        assertEquals(2, allGroups.size());
+        assertTrue(allGroups.containsAll(List.of(familyGroup, friendsGroup)));
+    }
+
+    @Test
+    void whenGroupWithIdExists_thenReturnGroupOnGetById() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findById(group.getId())).thenReturn(Optional.of(group));
+        // when
+        Group returnedGroup = service.getById(group.getId());
+        // then
+        assertEquals(returnedGroup, group);
+    }
+
+    @Test
+    void whenGroupWithIdDoesNotExist_thenThrowExceptionOnGetById() {
+        // given
+        String id = "nonExistingId";
+        // when
+        Mockito.when(repository.findById(id)).thenReturn(Optional.empty());
+        // then
+        assertThrows(IOException.class, () -> service.getById(id));
+    }
+
+    @Test
+    void whenGroupWithNameExists_thenReturnGroupOnGetByName() throws IOException {
+        // given
+        Group group = Group.builder().name("family").frequencyInDays(DEFAULT_FREQUENCY).build();
+        Mockito.when(repository.findByName(group.getName())).thenReturn(Optional.of(group));
+        // when
+        Group returnedGroup = service.getByName(group.getName());
+        // then
+        assertEquals(returnedGroup, group);
+    }
+
+    @Test
+    void whenGroupWithNameDoesNotExist_thenThrowExceptionOnGetByName() {
+        // given
+        String name = "nonExistingName";
+        // when
+        Mockito.when(repository.findByName(name)).thenReturn(Optional.empty());
+        // then
+        assertThrows(IOException.class, () -> service.getByName(name));
+    }
+    //endregion
+
 }
