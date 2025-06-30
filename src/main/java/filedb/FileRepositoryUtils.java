5b6b32116db344c9b733d9c28b11325b4773c8d3
Sharib Jafari <sharib.jafari.extern@idealo.de>
2025-06-30 14:36:18 +0200

    refactor: update groups.csv format and change FileRepositoryUtils class to public

--- src/main/java/filedb/FileRepositoryUtils.java
+++ src/main/java/filedb/FileRepositoryUtils.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class FileRepositoryUtils {
+public class FileRepositoryUtils {
 
     private FileRepositoryUtils() {
     }
